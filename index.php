94d054b5a8c186b7cd938d8586f68d3808e54fa9
rawka <info@rostechs.ru>
2015-12-19 19:59:45 +0400

    add structure to index

--- index.php
+++ index.php
@@ -13,6 +13,33 @@ defined('_JEXEC') or die;
 <head>
 </head>
 <body>
+    <div id="page-wrapper">
+        <div id="header">
+            <div id="logo"></div>
+            <div id="address"></div>
+            <div id="contacts"></div>
+        </div>
+        <div id="top-menu"></div>
+        <div id="content-wrapper">
+            <div id="content">
+                <div id="workarea-wrapper">
+                    <div id="left-menu"></div>
+                    <div id="banner"></div>
+                    <div id="workarea">
+                        <div id="workarea-inner"></div>
+                    </div>
+                </div>
+            </div>
+            <div id="sidebar">
+                <div id="sidebar-inner"></div>
+            </div>
+        </div>
+        <div id="space-for-footer"></div>
+    </div>
+    <div id="footer">
+        <div id="copyright"></div>
+        <div id="bottom-menu"></div>
+    </div>
 </body>
 </html>
 
